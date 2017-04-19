1f8862201c581a2dc76a234978ad059d53f4aeff
TodMcChesney <codescribe.net@gmail.com>
2017-04-19 07:46:15 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// Constructor function for cat instances
+var Cat = function(name, imageURL, counterID) {
+    this.name = name;
+    this.imageURL = imageURL;
+    this.counterID = counterID;
+};
+
+// Render the html for each cat instance
+Cat.prototype.renderHTML = function() {
+    var catDiv = document.createElement('div');
+    catDiv.setAttribute('class', 'center');
+    document.querySelector('.flex-container').appendChild(catDiv);
+
+    var h2 = document.createElement('h2');
+    var nameText = document.createTextNode(this.name);
+    h2.appendChild(nameText);
+    catDiv.appendChild(h2);
+
+    var img = document.createElement('img');
+    img.setAttribute('src', this.imageURL);
+    img.setAttribute('id', this.name);
+    img.setAttribute('alt', 'cat');
+    catDiv.appendChild(img);
+
+    var h3 = document.createElement('h3');
+    var counterText = document.createTextNode('Click this Cat to Play!');
+    h3.setAttribute('id', this.counterID);
+    h3.appendChild(counterText);
+    catDiv.appendChild(h3);
+};
+
+// Event listener that counts clicks on each cat image
+Cat.prototype.clickCounter = function () {
+    var catImage = document.getElementById(this.name);
+    var counter = document.getElementById(this.counterID);
+    var numClicks = 0;
+
+    catImage.addEventListener('click', function() {
+        numClicks += 1;
+        counter.textContent = 'Number of Clicks: ' + numClicks;
+    });
+};
+
+// Create cat #1 instance
+var cutie = new Cat('Cutie', 'img/cat-1.jpg', 'cat-1');
+cutie.renderHTML();
+cutie.clickCounter();
+
+// Create cat #2 instance
+var sneaky = new Cat('Sneaky', 'img/cat-2.jpg', 'cat-2');
+sneaky.renderHTML();
+sneaky.clickCounter();

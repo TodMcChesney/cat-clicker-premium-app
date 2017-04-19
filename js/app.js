3222d2cc4cbd9eb01fbae7a8b1c5ba40b8025d60
TodMcChesney <codescribe.net@gmail.com>
2017-04-19 09:41:06 +0200

    feat: Add renderCatLI() to render cat link list
    clickCatLink() listens and fires renderCatHTML() and clickCounter() when
    a cat's name is clicked.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 // Constructor function for cat instances
-var Cat = function(name, imageURL, counterID) {
+var Cat = function(name, imageURL, counterID, listID) {
     this.name = name;
     this.imageURL = imageURL;
     this.counterID = counterID;
+    this.listID = listID;
+};
+
+// Render the link for each cat
+Cat.prototype.renderCatLI = function () {
+    var catList = document.getElementById('cat-list');
+
+    var li = document.createElement('li');
+    var linkText = document.createTextNode(this.name);
+    li.setAttribute('id', (this.listID));
+    li.appendChild(linkText);
+    catList.appendChild(li);
+
+    this.clickCatLink();
 };
 
 // Render the html for each cat instance
-Cat.prototype.renderHTML = function() {
-    var catDiv = document.createElement('div');
-    catDiv.setAttribute('class', 'center');
-    document.querySelector('.flex-container').appendChild(catDiv);
+Cat.prototype.renderCatHTML = function() {
+    var catDiv = document.querySelector('.cat-div');
 
     var h2 = document.createElement('h2');
     var nameText = document.createTextNode(this.name);
@@ -41,12 +53,21 @@ Cat.prototype.clickCounter = function () {
     });
 };
 
+// Event listener for clicks on list of cats
+Cat.prototype.clickCatLink = function() {
+    var catLink = document.getElementById(this.listID);
+    var self = this;
+    catLink.addEventListener('click', function() {
+        document.querySelector('.cat-div').innerHTML = '';
+        self.renderCatHTML();
+        self.clickCounter();
+    });
+};
+
 // Create cat #1 instance
-var cutie = new Cat('Cutie', 'img/cat-1.jpg', 'cat-1');
-cutie.renderHTML();
-cutie.clickCounter();
+var cutie = new Cat('Cutie', 'img/cat-1.jpg', 'cat-1', '1');
+cutie.renderCatLI();
 
 // Create cat #2 instance
-var sneaky = new Cat('Sneaky', 'img/cat-2.jpg', 'cat-2');
-sneaky.renderHTML();
-sneaky.clickCounter();
+var sneaky = new Cat('Sneaky', 'img/cat-2.jpg', 'cat-2', '2');
+sneaky.renderCatLI();

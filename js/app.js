76e9cd33bbaf1dc7a6292e10de9cd3f228112137
TodMcChesney <codescribe.net@gmail.com>
2017-04-20 12:20:52 +0200

    feat: Add 'model' object with all cats data

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// Model (all cat data)
+var model = {
+    cats: [
+        {
+            name: 'Cutie',
+            image: 'img/cat-1.jpg',
+            clickCount: 0
+        },
+        {
+            name: 'Sneaky',
+            image: 'img/cat-2.jpg',
+            clickCount: 0
+        },
+        {
+            name: 'Cuddly',
+            image: 'img/cat-3.jpg',
+            clickCount: 0
+        },
+        {
+            name: 'Sleepy',
+            image: 'img/cat-4.jpg',
+            clickCount: 0
+        },
+        {
+            name: 'Grumpy',
+            image: 'img/cat-5.jpg',
+            clickCount: 0
+        }
+    ]
+};
